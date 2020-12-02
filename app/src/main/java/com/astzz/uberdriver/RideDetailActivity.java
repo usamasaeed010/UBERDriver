7cb855abff2aa24e13b2aa4f948534aa027c728d
usama khan <hafizahmed420@gmail.com>
2020-12-02 14:03:08 +0500

    n

--- app/src/main/java/com/astzz/uberdriver/RideDetailActivity.java
+++ app/src/main/java/com/astzz/uberdriver/RideDetailActivity.java
@@ -9,7 +9,7 @@ public class RideDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride_detail);
+        setContentView(R.layout.select_vihicle);
 
 
 
